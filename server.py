cb136738d29d29dd0558e3bc4b04ac6870c2901b
Hao Gao <hgao@twitter.com>
2013-11-13 03:24:37 -0500

    add jsonschema

--- server.py
+++ server.py
@@ -1,12 +1,31 @@
 from flask import Flask, request, jsonify
+from jsonschema import validate
+
 app = Flask(__name__)
 
+schema = {
+    "title": "SendHub Challenge Schema",
+    "type": "object",
+    "properties": {
+        "message": {
+            "type": "string"
+        },
+        "recipients": {
+            "type": "array",
+            "minItems": 1,
+            "items": {"type": "string"},
+            "uniqueItems": True
+        }
+    },
+    "required": ["message", "recipients"]
+}
+
 table = {
-    'small' : {'category_name' : 'Small', 'ip_subnets' : '10.0.1.', 'throughput' : 1, 'cost' : 0.01},
-    'medium' : {'category_name' : 'Medium', 'ip_subnets' : '10.0.2.', 'throughput' : 5, 'cost' : 0.05},
-    'large' : {'category_name' : 'Large', 'ip_subnets' : '10.0.3.', 'throughput' : 10, 'cost' : 0.1},
-    'super' : {'category_name' : 'Super', 'ip_subnets' : '10.0.4.', 'throughput' : 25, 'cost' : 0.25},
-    }
+    'small': {'category_name': 'Small', 'ip_subnets': '10.0.1.', 'throughput': 1, 'cost': 0.01},
+    'medium': {'category_name': 'Medium', 'ip_subnets': '10.0.2.', 'throughput': 5, 'cost': 0.05},
+    'large': {'category_name': 'Large', 'ip_subnets': '10.0.3.', 'throughput': 10, 'cost': 0.1},
+    'super': {'category_name': 'Super', 'ip_subnets': '10.0.4.', 'throughput': 25, 'cost': 0.25},
+}
 
 categories = ['super', 'large', 'medium', 'small']
 
@@ -17,6 +36,7 @@ def hello():
 @app.route('/api/sendhub/v1.0/routes', methods=['POST'])
 def routing_service():
     data = request.json
+    validate(data, schema)
     recipients = data['recipients']
     recipients_num = len(recipients)
     routes = []
@@ -27,13 +47,11 @@ def routing_service():
             for j in range(1, requests_needed + 1):
                 ip = table[categories[i]]['ip_subnets'] + str(j)
                 end = start + table[categories[i]]['throughput']
-                routes.append({'ip' : ip, 'recipients' : recipients[start : end]})
+                routes.append({'ip': ip, 'recipients': recipients[start : end]})
                 start = end
         recipients_num = recipients_num % table[categories[i]]['throughput']
-    return jsonify({'message' : 'SendHubRocks', 'routes': routes})
+    return jsonify({'message': 'SendHubRocks', 'routes': routes})
 
 if __name__ == '__main__':
     #app.run(debug=True)
     pass
-
-# curl -X POST -H "Content-Type: application/json" -d '{"message": "SendHub Rocks", "recipients": ["+15555555556","+15555555555","+15555555554","+15555555553","+15555555552","+15555555551"]}' http://127.0.0.1:5000/api/sendhub/v1.0/routes
